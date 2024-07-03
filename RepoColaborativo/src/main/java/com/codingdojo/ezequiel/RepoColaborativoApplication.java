42544395ece95a33a3816c41d850d55b67ee9370
Matias Mamani <matias.ezequiel.1177@gmail.com>
2024-07-03 20:05:58 -0300

    First commit

---
+++ RepoColaborativo/src/main/java/com/codingdojo/ezequiel/RepoColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ezequiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoColaborativoApplication.class, args);
+	}
+
+}
---
+++ RepoColaborativo/src/test/java/com/codingdojo/ezequiel/RepoColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ezequiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
